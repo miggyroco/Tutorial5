0a0a3bad8cbc052d1c6e9ada31931d5a64e2a6e9
ADMINRG-TTD989C\office <miggyroco@gmail.com>
2019-09-23 14:59:27 +0800

    testing iterator

--- Training49 - Implementing Iterable/src/AppImplementingIterable.java
+++ Training49 - Implementing Iterable/src/AppImplementingIterable.java
@@ -1,9 +1,49 @@
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class AppImplementingIterable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		LinkedList<String> animals = new LinkedList<String>();
+		
+		animals.add("kitsune");
+		animals.add("neko");
+		animals.add("inu");
+		animals.add("usagi");
+		
+		
+		Iterator<String> it = animals.iterator();
+		
+				//hasNext if there are nextitem on the list
+		while(it.hasNext()) {
+			
+			//start in the first item of the list
+			String value = it.next();
+			
+			System.out.println(value);
+			
+			if(value.equals("neko")) {
+				
+				it.remove();
+			}
+			
+		}
+		
+		System.out.println();
+		
+		
+		
+		// Modern iteration, java 5 and later
+		
+		
+		for(String animal: animals) {
+			
+			System.out.println(animal);
+		}
+		
+		
+		
 	}
 
 }
