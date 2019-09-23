3876ef41fd782678eb8b2f7c887d7408f73050b0
ADMINRG-TTD989C\office <miggyroco@gmail.com>
2019-09-23 15:35:10 +0800

    iterators

--- Training48 - Iterators/src/AppIterators.java
+++ Training48 - Iterators/src/AppIterators.java
@@ -1,10 +1,49 @@
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class AppIterators {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
 
+		LinkedList<String> animals = new LinkedList<String>();
+		
+		animals.add("kitsune");
+		animals.add("neko");
+		animals.add("inu");
+		animals.add("usagi");
+		
+		
+		Iterator<String> it = animals.iterator();
+		
+				//hasNext if there are next item on the list
+		while(it.hasNext()) {
+			
+			//start in the first item of the list
+			String value = it.next();
+			
+			System.out.println(value);
+			
+			if(value.equals("neko")) {
+				
+				it.remove();
+			}
+			
+		}
+		
+		System.out.println();
+		
+		
+		
+		// Modern iteration, java 5 and later
+		
+		
+		for(String animal: animals) {
+			
+			System.out.println(animal);
+		}
+		
+		
 	}
 
 }
