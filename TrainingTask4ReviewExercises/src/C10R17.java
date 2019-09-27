3aa09ce8eb2aea16f4cd2f8ce202fe60d790c1df
ADMINRG-TTD989C\office <cholo@lapapa88.com>
2019-09-27 15:32:22 +0800

    re arrange

--- TrainingTask4ReviewExercises/src/C10R17.java
+++ TrainingTask4ReviewExercises/src/C10R17.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class C10R17 {
 
+	public static double result = 0;
+	
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 		Scanner scanner1 = new Scanner(System.in);
-		Scanner scanner2 = new Scanner(System.in);
+		
 
-		double num1 = 0, num2 = 0, result = 0;
+		double num1 = 0, num2 = 0;
+//		, result = 0;
+		
 		char opt = 0;
 		int loop = 0;
 
@@ -24,21 +28,7 @@ public class C10R17 {
 
 			Display_Menu(num1, num2, opt);
 
-			opt = scanner2.next().charAt(0);
-
-			if (opt == 'm' || opt == 'M') {
-				result = Multiplication(num1, num2);
-			} else if (opt == 'd' || opt == 'D') {
-				result = Division(num1, num2);
-			} else if (opt == 'a' || opt == 'A') {
-				result = Addition(num1, num2);
-			} else if (opt == 's' || opt == 'S') {
-				result = Subtraction(num1, num2);
-			} else if (opt == 'q' || opt == 'Q') {
-				System.exit(0);
-			} else {
-				System.out.println("invalid input");
-			}
+			
 
 			System.out.println("The result of this operation is " + result);
 
@@ -52,6 +42,8 @@ public class C10R17 {
 	}
 
 	public static void Display_Menu(double num1, double num2, char opt) {
+		Scanner scanner2 = new Scanner(System.in);
+		
 		System.out.println();
 		System.out.println("<< Math Operation Menu >>\n");
 		System.out.println("First Number : " + num1 + " " + "Second Number : " + num2);
@@ -59,6 +51,22 @@ public class C10R17 {
 				+ "S - Subtraction \r\n" + "Q - Quit ");
 
 		System.out.print("\nSelect an option: ");
+		
+		opt = scanner2.next().charAt(0);
+
+		if (opt == 'm' || opt == 'M') {
+			result = Multiplication(num1, num2);
+		} else if (opt == 'd' || opt == 'D') {
+			result = Division(num1, num2);
+		} else if (opt == 'a' || opt == 'A') {
+			result = Addition(num1, num2);
+		} else if (opt == 's' || opt == 'S') {
+			result = Subtraction(num1, num2);
+		} else if (opt == 'q' || opt == 'Q') {
+			System.exit(0);
+		} else {
+			System.out.println("invalid input");
+		}
 
 	}
 
@@ -66,6 +74,9 @@ public class C10R17 {
 		System.out.println("\n\nYou have chosen Multiplication option\n");
 		return num1 * num2;
 	}
+	
+	
+	
 	public static double Division(double num1, double num2) {
 		System.out.println("You have chosen Division option\n");
 		return num1 / num2;
